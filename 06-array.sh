d8b27cd9f186b69ef77b65f497484e415bd63b85
varalaxmi <varadevops2121@gmail.com>
2024-04-17 18:28:54 +0530

    hellow script

---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES=("RRR" "Chanti" "murari")
+
+#size of above array is 3
+#index are 0,1,2
+
+#list always start with 0
+
+echo "first movie is: ${MOVIE[0]}"
+echo "first movie is: ${MOVIE[1]}"
+echo "first movie is: ${MOVIE[@]}"
