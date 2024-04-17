7e93a86c188755a7cac2d8d0f0631c15036c12b2
varalaxmi <varadevops2121@gmail.com>
2024-04-17 17:07:40 +0530

    hellow script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hellow suresh how are you"
+echo "Suresh:: Hai Ramesh Iam fine, how are you"
+echo "Ramesh:: Iam fine too how is your work"
+echo "Suresh:: not bad, Iam thinking to upgrade to devops"
