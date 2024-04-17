49741e7cac0c998f3d79c9103602e2b5f3c3ce27
varalaxmi <varadevops2121@gmail.com>
2024-04-17 17:49:25 +0530

    hellow script

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1 #no space between and = values
+PERSON2=$2
+
+
+echo "$PERSON1:: Hellow $PERSON2 how are you"
+echo "$PERSON2:: Hai $PERSON1 Iam fine, how are you"
+echo "$PERSON1:: Iam fine too how is your work"
+echo "$PERSON2:: not bad, Iam thinking to upgrade to devops"
