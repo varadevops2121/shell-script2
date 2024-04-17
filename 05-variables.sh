4d00ddec1b59f2426c60a0bc440f198c755cd225
varalaxmi <varadevops2121@gmail.com>
2024-04-17 18:06:34 +0530

    hellow script

---
+++ 05-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "plese enter the user name::"
+
+read USERNAME #here username is varable
+
+echo "plese enter password::"
+
+read PASSWORD
+
+echo "Username is $USERNAME, Password is $PASSWORD"
+
+
+
+
+
