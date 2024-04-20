46584ff9fb5f3958ec02f8407354ea538582fa62
varalaxmi <varadevops2121@gmail.com>
2024-04-20 20:51:22 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,13 +4,21 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "script started executing at: $TIMESTAMP"
+
+
+
 VALIDATE(){
     if [ $1 -ne 0 ]
      then 
-     echo "$2...FAILURE"
+     echo -e "$2...$R FAILURE $N"
      exit 1
     else
-     echo "$2...SUCCESS"  
+     echo -e "$2...$G SUCCESS $N"  
     fi
 }
 
