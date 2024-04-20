c97ac5c7b4362576513bcd96d46578aa25930e76
varalaxmi <varadevops2121@gmail.com>
2024-04-20 20:34:39 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
      then 
      echo "$2...FAILURE"
      exit 1
@@ -19,8 +22,8 @@ else
     echo "you are super user"
 fi    
 
-dnf install mysql -y 
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "installing MySQL"
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "installing git"
